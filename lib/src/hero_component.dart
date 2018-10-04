373b0e16bd1e516191dd72218e731e3cd17e571c
Lance Newman <lance.newman@elynxx.com>
2018-10-04 11:09:44 -0400

    completed tour of heroes tutorial for angular dart.

---
+++ lib/src/hero_component.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_router/angular_router.dart';
+
+import 'hero.dart';
+import 'hero_service.dart';
+import 'route_paths.dart';
+
+@Component(
+  selector: 'my-hero',
+  directives: [coreDirectives, formDirectives],
+  templateUrl: 'hero_component.html',
+  styleUrls: ['hero_component.css']
+)
+class HeroComponent implements OnActivate {
+  final HeroService _heroService;
+  final Location _location;
+
+  Hero hero;
+
+  HeroComponent(this._heroService, this._location);
+ 
+  @override
+  void onActivate(RouterState previous, RouterState current) async {
+    final id = getId(current.parameters);
+
+    if(id != null) hero = await (_heroService.get(id));
+  }
+
+  int getId(Map<String, String> parameters) {
+    final id = parameters[idParam];
+    
+    return id == null ? null : int.tryParse(id);
+  }
+
+  void goBack() => _location.back();
+  
+  Future<void> save() async {
+    await _heroService.update(hero);
+    goBack();
+  }
+}
--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,20 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:angular_app/app_component.template.dart' as ng;
+import 'package:angular_app/in_memory_data_service.dart';
+import 'package:http/http.dart';
+
+import 'main.template.dart' as self;
+
+@GenerateInjector([
+  routerProvidersHash,
+  ClassProvider(Client, useClass: InMemoryDataService),
+  // using a real back end?
+  // import 'package:http/browser_client.dart' and change the above to:
+  // ClassProvider(BrowserClient),
+])
+final InjectorFactory injector = self.injector$Injector;
 
 void main() {
-  runApp(ng.AppComponentNgFactory);
+  runApp(ng.AppComponentNgFactory, createInjector: injector);
 }
